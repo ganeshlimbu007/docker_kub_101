525849dc466cde3e3f25809dead2415168ceaa51
ganeshlimbu007 <57285671+ganeshlimbu007@users.noreply.github.com>
2025-12-17 15:04:15 +0545

    docker update fixes for BE

--- 04_multi_container_apps/backend/app.js
+++ 04_multi_container_apps/backend/app.js
@@ -83,8 +83,10 @@ app.delete("/goals/:id", async (req, res) => {
   }
 });
 
+const mongoUri = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URI}`;
+console.log("MONGO URI:", mongoUri);
 mongoose.connect(
-  "mongodb://max:secret@mongodb:27017/course-goals?authSource=admin",
+  mongoUri,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -94,7 +96,7 @@ mongoose.connect(
       console.error("FAILED TO CONNECT TO MONGODB");
       console.error(err);
     } else {
-      console.log("CONNECTED TO MONGODB");
+      console.log("CONNECTED TO MONGODB !");
       app.listen(80);
     }
   }
